23e0b2aadfd3fee24ea03b23da3222c38633bf92
Sergio Aparecido <sergiopxp@gmail.com>
2023-10-18 23:39:07 -0300

    Criada solucao MVC para venda de lanches. Introducao de um carrossel e relacionamento entre entidades Lanches e Categorias

---
+++ QuickFood/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuickFood.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
